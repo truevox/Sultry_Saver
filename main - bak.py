4a5f55419bc9bff382abbe5660ba2a3cdb8919fb
Marvin Bentley II <truevox@gmail.com>
2018-07-15 16:08:56 -0400

    Initial Commit - Some combined code, main.py is current WIP Sultry_$aver

---
+++ main - bak.py
@@ -0,0 +1,113 @@
+import time
+import random
+import microcontroller
+import busio
+import sys
+import board
+from adafruit_circuitplayground.express import cpx
+
+
+# This is a special command that will cause a single-press RESET to go
+# into bootloader more (instead of double-click) to make it easier for
+# MakeCode-rs who don't intend to use CircuitPython!
+# microcontroller.on_next_reset(microcontroller.RunMode.BOOTLOADER)
+
+# Set this to True to turn on the capacitive touch tones
+TOUCH_PIANO = True
+
+# NeoPixel color names
+WHITE = (50, 50, 50)
+OFF   = (0,   0,  0)
+
+# Not too bright!
+cpx.pixels.brightness = 0.3
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if (pos < 0) or (pos > 255):
+        return (0, 0, 0)
+    if pos < 85:
+        return (int(255 - pos*3), int(pos*3), 0)
+    elif pos < 170:
+        pos -= 85
+        return (0, int(255 - (pos*3)), int(pos*3))
+    else:
+        pos -= 170
+    return (int(pos*3), 0, int(255 - pos*3))
+
+cpx.play_file("Coin.wav")   # Play a coin sound on boot
+
+# Set up the accelerometer to detect tapping
+cpx.detect_taps = 1    # detect single tap only
+
+# Our counter for all 10 pixels
+pixeln = 0
+# We can tell the switch changed
+last_switch = cpx.switch
+
+
+
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+ 
+uart = busio.UART(board.TX, board.RX, baudrate=9600)
+
+ 
+while True:
+    data = uart.read(32)  # read up to 32 bytes
+    # print(data)  # this is a bytearray type
+ 
+    if data is not None:
+        cpx.red_led = True
+ 
+        # convert bytearray to string
+        data_string = ''.join([chr(b) for b in data])
+        print(data_string, end="")
+ 
+        cpx.red_led = False
+
+
+
+ #   data = sys.stdin.read(1)
+    if data == 1:
+        cpx.red_led = True
+
+    elif data != 0:
+        cpx.red_led = False
+
+
+
+
+
+    #cpx.red_led = True                  # Turns the little LED next to USB on
+    # if cpx.
+    #cpx.pixels[1] = 100
+
+    if TOUCH_PIANO:
+        if cpx.touch_A4:              #  If we set to play tones
+            cpx.pixels[1] = 50 # cpx.start_tone(524)
+        elif cpx.touch_A5:
+            cpx.pixels[2] = 100 # cpx.start_tone(588)
+        elif cpx.touch_A6:
+            cpx.pixels[3] = 125 # cpx.start_tone(660)
+        elif cpx.touch_A7:
+            cpx.pixels[4] = 150 # cpx.start_tone(698)
+        elif cpx.touch_A1:
+            cpx.pixels[5] = 175 # cpx.start_tone(784)
+        elif cpx.touch_A2:
+            cpx.pixels[6] = 200 # cpx.start_tone(880)
+        elif cpx.touch_A3:
+            cpx.pixels[7] = 225 # cpx.start_tone(988)
+        else:
+            for p in range(10):     #cpx.stop_tone()           # nothing touched? turn off the audio
+                #color = wheel(25 * ((pixeln + p)%10))
+                cpx.pixels[p] = 0
+                # Every time we go around, print sensor data
+                print("Temperature: %0.1f *C" % cpx.temperature)
+                print("Light Level: %d" % cpx.light)
+                x, y, z = cpx.acceleration
+                print("Accelerometer: (%0.1f, %0.1f, %0.1f) m/s^2" % (x, y, z))
+                print('-' * 40)
+
+    # loop to the beginning!
---
+++ main.py
@@ -0,0 +1,188 @@
+import time
+import random
+import microcontroller
+import busio
+import sys
+import board
+from adafruit_circuitplayground.express import cpx
+
+
+#try:
+#    # your code
+#except KeyboardInterrupt:
+#    sys.exit(0) # or 1, or whatever
+
+# This is a special command that will cause a single-press RESET to go
+# into bootloader more (instead of double-click) to make it easier for
+# MakeCode-rs who don't intend to use CircuitPython!
+# microcontroller.on_next_reset(microcontroller.RunMode.BOOTLOADER)
+
+# Set this to True to turn on the capacitive touch tones
+
+# NeoPixel color names
+WHITE = (50, 50, 50)
+OFF = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+
+# Not too bright!
+#cpx.pixels.brightness = 0.2
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if (pos < 0) or (pos > 255):
+        return (0, 0, 0)
+    if pos < 85:
+        return (int(255 - pos*3), int(pos*3), 0)
+    elif pos < 170:
+        pos -= 85
+        return (0, int(255 - (pos*3)), int(pos*3))
+    else:
+        pos -= 170
+    return (int(pos*3), 0, int(255 - pos*3))
+    
+def Temp_convert(temp, unit):
+    unit = unit.lower()
+    if unit == "c":
+        temp = 9.0 / 5.0 * temp + 32
+        return temp #"%s degrees Fahrenheit"% temp
+    if unit == "f":
+        temp = (temp - 32)  / 9.0 * 5.0
+        return temp #"%s degrees Celsius"% temp
+
+def scale(val, src, dst):
+    """
+    Scale the given value from the scale of src (SouRCe) to the scale of dst (DeSTination).
+    """
+    return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
+
+ #print scale(0, (0.0, 99.0), (-1.0, +1.0))
+ #print scale(1, (0.0, 99.0), (-1.0, +1.0))
+ #print scale(99, (0.0, 99.0), (-1.0, +1.0))
+
+
+cpx.play_file("Coin.wav")   # Play a coin sound on boot
+
+# Set up the accelerometer to detect tapping
+cpx.detect_taps = 1    # detect single tap only
+
+# Our counter for all 10 pixels
+pixeln = 0
+# We can tell the switch changed
+last_switch = cpx.switch
+
+
+
+#led = digitalio.DigitalInOut(board.D13)
+#led.direction = digitalio.Direction.OUTPUT
+ 
+uart = busio.UART(board.TX, board.RX, baudrate=9600)
+
+
+Test_lights = cpx.pixels
+
+while True:
+
+    My_Debug = cpx.switch
+
+    if My_Debug == True:
+        # Water_temp = (((Temp_convert(cpx.temperature,"c") * .05 ) * 4**5) - 4260)
+        Water_temp = Temp_convert(cpx.temperature,"c")
+        while cpx.touch_A1:
+            Water_temp = 40.0
+        while cpx.touch_A2:
+            Water_temp = 65.0
+        while cpx.touch_A3:
+            Water_temp = 85.0
+        while cpx.touch_A4:
+            Water_temp = 105.0
+        while cpx.touch_A5:
+            Water_temp = 115.0
+        while cpx.touch_A6:
+            Water_temp = 125.0
+        while cpx.touch_A7:
+            Water_temp = 145.0
+
+    elif My_Debug == False:
+        Water_temp = Temp_convert(cpx.temperature,"c")
+
+
+    while Water_temp > -160.0:
+        cpx.pixels[0] = WHITE
+       # Test_lights[0] = WHITE
+       # Test_lights[1:9] = OFF
+    while Water_temp > 60.0:
+        cpx.pixels[1] = BLUE
+       # Test_lights[2] = WHITE
+       # Test_lights[2:] = OFF
+    while Water_temp > 80.0:
+        cpx.pixels[2] = GREEN
+    while Water_temp > 100.0:
+        cpx.pixels[3] = PURPLE
+    while Water_temp > 110.0:
+        cpx.pixels[4] = YELLOW
+    while Water_temp > 120.0:
+        cpx.pixels[5] = YELLOW
+    while Water_temp > 130.0:
+        cpx.pixels[6] = RED
+    while Water_temp > 140.0:
+        cpx.pixels[7] = RED
+    #else:
+    #    cpx.pixels.fill = ((0,0,0))
+
+    if Water_temp > 120:
+        cpx.red_led = True
+    elif Water_temp <= 120:
+        cpx.red_led = False
+
+    time.sleep(1)
+    print(Water_temp)
+    print("Debugging is set to", My_Debug, "and temp readings are", not My_Debug)
+    cpx.pixels.fill = OFF
+    
+    if Water_temp > 120:
+        cpx.red_led = True
+    elif Water_temp <= 120:
+        cpx.red_led = False
+
+
+
+
+
+    '''
+    data = uart.read(32)  # read up to 32 bytes
+    # print(data)  # this is a bytearray type
+ 
+    if data is not None:
+        cpx.red_led = True
+ 
+        # convert bytearray to string
+        data_string = ''.join([chr(b) for b in data])
+        print(data_string, end="")
+ 
+        cpx.red_led = False
+
+
+
+ #   data = sys.stdin.read(1)
+    if data == 1:
+        cpx.red_led = True
+
+    elif data != 0:
+        cpx.red_led = False
+
+
+
+
+
+    #cpx.red_led = True                  # Turns the little LED next to USB on
+    # if cpx.
+    #cpx.pixels[1] = 100
+
+
+    # loop to the beginning!'''
